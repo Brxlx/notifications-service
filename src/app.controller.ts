53a04b0ab294acaaaeee3461d158da0451d4487c
Bruno Santos <bruno.santos@allin.com.br>
2022-12-13 12:02:55 -0300

    feat: prsima client; list and post notifications

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { randomUUID } from 'node:crypto';
 
-@Controller()
+import { Controller, Get, Post } from '@nestjs/common';
+
+import { PrismaService } from './database/prisma/prisma.service';
+
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create() {
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content: 'Você tem uma nova solicitação de amizade!',
+        category: 'social',
+        recipientId: randomUUID(),
+      },
+    });
   }
 }
